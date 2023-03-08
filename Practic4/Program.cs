0447ead52b21f31812bf52965eb3e5c9d335fb3a
alexandr.kerzhakov35 <alexandr.kerzhakov35@gmail.com>
2023-03-08 15:18:12 +0300

    homework1

---
+++ Practic4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Все четные до N
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32( Console.ReadLine() );
+int number = 1;
+
+while(number <= N) {
+    if (number %2 == 0) {
+        Console.Write(number + " ");
+    }
+    number++;
+}
+
+
+
+
