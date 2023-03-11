9588cfc372ded1eb13f2b48136de54fe4b427809
alexandr.kerzhakov35 <alexandr.kerzhakov35@gmail.com>
2023-03-11 11:55:10 +0300

    Add Seminar1 & Seminar2

---
+++ Seminar1/Practic2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Максимум из 3-х чисел
+
+Console.WriteLine("Введите 3 числа, после каждого нажмите enter");
+
+int number1 = Convert.ToInt32( Console.ReadLine() );
+int number2 = Convert.ToInt32( Console.ReadLine() );
+int number3 = Convert.ToInt32( Console.ReadLine() );
+
+int max = number1;
+
+if (max > number2) {
+    if (max > number3) {
+
+    }
+    else {
+        max = number3;
+    }
+}
+else {
+    max = number2;
+    if (max > number3) {
+        
+    }
+    else {
+        max = number3;
+    }
+}
+Console.WriteLine("Максимальное число: " + max);
---
+++ Seminar2/Zad13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32( Console.ReadLine() );
+int third = 0;
+
+if (number/100 > 0) {
+    while (number/100 > 0) {
+        third = number%10;
+        // Console.Write($"{third} ");
+        number = number/10;
+        // Console.Write($"{number} ");
+    }
+    Console.WriteLine($"Третья цифра числа: {third} ");
+} else {
+    Console.WriteLine("Третьей цифры нет");
+    }
+
