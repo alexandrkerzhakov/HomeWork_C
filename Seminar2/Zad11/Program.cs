a675a7bb5b962ed71ab0be04258e56c6541a7934
alexandr.kerzhakov35 <alexandr.kerzhakov35@gmail.com>
2023-03-11 13:00:53 +0300

    last update

--- Seminar2/Zad11/Program.cs
+++ Seminar2/Zad11/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На входе от пользователя целое число любой разрядности, надо удалить вторую цифру слева направо этого числа.
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32( Console.ReadLine() );
+int count_raz = 0;
+int[] tmp_raz = new int[32];
+
+while(N > 0) {
+    tmp_raz[count_raz] = N % 10;
+    // Console.Write(tmp_raz[count_raz]);
+    count_raz++;
+    N = N / 10;
+}
+Console.WriteLine();
+
+int[] raz = new int[count_raz];
+Array.Copy(tmp_raz, raz, count_raz);
+Array.Reverse(raz);
+Console.WriteLine();
+
+Console.Write("Число без 2-го разряда: ");
+if (raz.Length > 1) {
+    for (int i = 0; i < raz.Length; i++) {
+        if (i != 1) {
+        Console.Write(raz[i]);
+        }
+    }
+}
+else {
+        Console.WriteLine($"Число состоит из одного разряда: {raz[0]}");
+}
+
+
